b4953be38b25254ef0f9269fe212eac769a11076
McCorriston, Jeromy <illuzio571@yahoo.com>
2018-10-07 12:14:04 -0400

    Updated to use AccuWeather API

--- CanIGoOutside/CanIGoOutside/Controllers/HomeController.cs
+++ CanIGoOutside/CanIGoOutside/Controllers/HomeController.cs
@@ -1,19 +1,115 @@
-﻿using System;
+﻿using CanIGoOutside.Models;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using RestSharp;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Script.Serialization;
 
 namespace CanIGoOutside.Controllers
 {
     public class HomeController : Controller
     {
+        static HttpClient client = new HttpClient();
+        string locationCode;
+        CurrentCondition currentCondition;
+
+        [HttpGet]
         public ActionResult Index()
         {
-            string locationKey;
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(string zip)
+        {
+            GetLocationCode(zip);
+            if (locationCode != null)
+            {
+                GetCurrentCondition();
+
+                ViewBag.temp = currentCondition.Temperature;
+                ViewBag.windSpeed = currentCondition.WindSpeed;
+                ViewBag.humidity = currentCondition.RelativeHumidity;
+                ViewBag.rainChance = currentCondition.PrecipitationProbability;
+
+                if (CheckIfYouCanGoOutside())
+                {
+                    ViewBag.result = "Yes!";
+                }
+                else
+                {
+                    ViewBag.result = "No!";
+                }
+            }
+            else
+            {
+                ViewBag.isValid = "false";
+            }
 
             return View();
-            
+        }
+
+        private void GetLocationCode(string zip)
+        {
+            var client = new RestClient("http://dataservice.accuweather.com/locations/v1/postalcodes/search?apikey=HackPSU2018&q=" + zip);
+            var request = new RestRequest(Method.GET);
+            request.AddHeader("Postman-Token", "23acb8fb-c16a-4064-b29c-7d55d782675e");
+            request.AddHeader("Cache-Control", "no-cache");
+            IRestResponse response = client.Execute(request);
+
+            PostalCode postalCode = new PostalCode(response.Content.ToString());
+
+            locationCode = postalCode.Key;
+        }
+
+        private void GetCurrentCondition()
+        {
+            var client = new RestClient("http://dataservice.accuweather.com/forecasts/v1/hourly/1hour/" + locationCode + "?apikey=HackPSU2018&details=true&metric=false");
+            var request = new RestRequest(Method.GET);
+            request.AddHeader("Postman-Token", "23acb8fb-c16a-4064-b29c-7d55d782675e");
+            request.AddHeader("Cache-Control", "no-cache");
+            IRestResponse response = client.Execute(request);
+
+            currentCondition = new CurrentCondition(response.Content.ToString());
+        }
+
+        private bool CheckIfYouCanGoOutside()
+        {
+            if (currentCondition.Temperature < 50)
+            {
+                ViewBag.reason = "Too cold!";
+                return true;
+            }
+            else if (currentCondition.Temperature >= 90)
+            {
+                ViewBag.reason = "Too hot!";
+                return true;
+            }
+            else if (currentCondition.RainValue > 0)
+            {
+                ViewBag.reason = "It's raining!!!";
+                return true;
+            }
+            else if (currentCondition.SnowValue > 0)
+            {
+                ViewBag.reason = "It's snowing!!!";
+                return true;
+            }
+            else if (currentCondition.IceValue > 0)
+            {
+                ViewBag.reason = "There's ice!!!";
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
     }
 }
--- CanIGoOutside/CanIGoOutside/Models/CurrentCondition.cs
+++ CanIGoOutside/CanIGoOutside/Models/CurrentCondition.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,8 +8,30 @@ namespace CanIGoOutside.Models
 {
     public class CurrentCondition
     {
-        public Temperature Temperature { get; set; }
-        public Wind Wind { get; set; }
+        public CurrentCondition(string json)
+        {
+            if (json != null)
+            {
+                JArray jsonArray = JArray.Parse(json);
+                if (jsonArray != null)
+                {
+                    dynamic data = JObject.Parse(jsonArray[0].ToString());
+                    Temperature = data.Temperature.Value;
+                    WindSpeed = data.Wind.Speed.Value;
+                    RelativeHumidity = data.RelativeHumidity;
+                    PrecipitationProbability = data.PrecipitationProbability;
+                    RainValue = data.Rain.Value;
+                    SnowValue = data.Snow.Value;
+                    IceValue = data.Ice.Value;
+                }
+            }
+        }
+        public decimal Temperature { get; set; }
+        public decimal WindSpeed { get; set; }
         public decimal RelativeHumidity { get; set; }
+        public decimal PrecipitationProbability { get; set; }
+        public decimal RainValue { get; set; }
+        public decimal SnowValue { get; set; }
+        public decimal IceValue { get; set; }
     }
 }
